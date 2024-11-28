e5347a562d55c88aa2ca4138f1af90bfbfdc69f5
Yuri Visser <yurivisser7@gmail.com>
2024-11-28 15:59:42 -0400

    customization; magic warp route setup

--- src/consts/app.ts
+++ src/consts/app.ts
@@ -7,9 +7,9 @@ export const MAIN_FONT = SpaceGrotesk({
   preload: true,
   fallback: ['sans-serif'],
 });
-export const APP_NAME = 'Hyperlane Warp UI Template';
-export const APP_DESCRIPTION = 'A DApp for Hyperlane Warp Route transfers';
+export const APP_NAME = 'Treasure Warp UI';
+export const APP_DESCRIPTION = 'A DApp for Treasure Warp Route transfers';
 export const APP_URL = 'hyperlane-warp-template.vercel.app';
-export const BRAND_COLOR = Color.primary;
-export const BACKGROUND_COLOR = Color.primary;
+export const BRAND_COLOR = Color.red;
+export const BACKGROUND_COLOR = Color.black;
 export const BACKGROUND_IMAGE = 'url(/backgrounds/main.svg)';
--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -1,9 +1,37 @@
-import { WarpCoreConfig } from '@hyperlane-xyz/sdk';
+import { TokenStandard, WarpCoreConfig } from '@hyperlane-xyz/sdk';
 
 // A list of Warp Route token configs
 // These configs will be merged with the warp routes in the configured registry
 // The input here is typically the output of the Hyperlane CLI warp deploy command
 export const warpRouteConfigs: WarpCoreConfig = {
-  tokens: [],
+  tokens: [
+    {
+      chainName: 'arbitrumsepolia',
+      standard: TokenStandard.EvmHypCollateral,
+      decimals: 18,
+      symbol: 'MAGIC',
+      name: 'MAGIC',
+      addressOrDenom: '0x5a6f771Af6C395a9C1765AE1cF26aec4FC178683',
+      collateralAddressOrDenom: '0xe2faac0d4fd98a9b2275e5acd90d81fa0b6a4436',
+      connections: [
+        {
+          token: 'ethereum|treasuretopaz|0xae8D8A869bd5C6d9b8DD2EBEe2891b0313243211',
+        },
+      ],
+    },
+    {
+      chainName: 'treasuretopaz',
+      standard: TokenStandard.EvmHypNative,
+      decimals: 18,
+      symbol: 'MAGIC',
+      name: 'MAGIC',
+      addressOrDenom: '0xae8D8A869bd5C6d9b8DD2EBEe2891b0313243211',
+      connections: [
+        {
+          token: 'ethereum|arbitrumsepolia|0x5a6f771Af6C395a9C1765AE1cF26aec4FC178683',
+        },
+      ],
+    },
+  ],
   options: {},
 };
